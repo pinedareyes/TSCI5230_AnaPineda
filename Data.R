03ebf2306b7bc69e1a6d236a150909c1501c8943
AnaPineda <apineda@uthsc.edu>
2025-09-10 17:03:15 -0500

    Class 09/10 filter data based condition to create a new data frame

--- Data.R
+++ Data.R
@@ -35,6 +35,6 @@ data1<-full_join(data0[["encounters"]], data0[["procedures"]], data0[["condition
 fu<-data0[["conditions"]] %>%View # to view a data frame
 #How to find and extract data from a data frame----
 # Find matches for diabetes from a data set & extract it to create a new data
-  filter(data0[["conditions"]], grepl("\\bdiab",DESCRIPTION, ignore.case = TRUE))%>%#grep will let you know the row number were the value is true
-    with
+filter(data0[["conditions"]], grepl("\\bdiab",DESCRIPTION, ignore.case = TRUE))%>%#grep will let you know the row number were the value is true
+    with(data=.,list(patient=unique(PATIENT), encounter=unique(ENCOUNTER)))%>%View()
 
