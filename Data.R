9a2a6f8e7a3bff577851dc3a4d83ad9f4e300a2a
AnaPineda <apineda@uthsc.edu>
2025-09-10 16:48:32 -0500

    Class 3 9/10

--- Data.R
+++ Data.R
@@ -21,17 +21,20 @@ options(max.print=500);
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf)
 datasource <- "../output/csv/"
 #data0<-import(list.files(datasource,full.names = T) [9]) is to name files to identify/ specific files
-data0<-sapply(list.files(datasource,full.names = T),import) %>% #%>% it pipes form left to right
+data0<-sapply(list.files(datasource,full.names = T),import) %>% #%>% it is a pipe expression form left to right to continue a command in a new line
   #rename all the objects in data0 to get rid of the prefix "../output/csv/" and suffix ".csv"
   setNames(gsub(paste0("^",datasource,"/{0,1}|\\.csv$"),"",names(.))) #paste0 takes several variable and put them together. "gsubs" sub patterns
 #Notes:create_report(dataname ie data0[[file that you want the report from]])
 #inside a code a period is interpreted as . Therefore to be interpreted as a period (ie .com) you need to ad \\
-#data elements of interest for this project are: patient id, encounter class, description, encounter cost, reason description, type of treatment, type of insurance, from procedure: description, base cost, reason description ----
-
-#HW what columns you wish to have to be able to analyse (mainly from the encounter table) and add them to a new data frame using ?left_join
-data1<-full_join(data0[["encounters"]], data0[["procedures"]], by = c("PATIENT", "START", "STOP"), relationship = "many-to-many")
-
+#data elements of interest for this project are: patient id, encounter class, description, encounter cost, reason description, type of treatment, type of insurance, from procedure: description, base cost, reason description
+#HW what columns you wish to have to be able to analyse (mainly from the encounter table) and add them to a new data frame using ?left_join----
+data1<-full_join(data0[["encounters"]], data0[["procedures"]], data0[["conditions"]], by = c("PATIENT", "START", "STOP"), relationship = "many-to-many")
 # keep only specific columns
 #data1 <- data1 %>%
-#. select(PATIENT, START, STOP, diagnosis, procedure_code)
+#  select(PATIENT, START, STOP, PROCEDURE_CODE)
+fu<-data0[["conditions"]] %>%View # to view a data frame
+#How to find and extract data from a data frame----
+# Find matches for diabetes from a data set & extract it to create a new data
+  filter(data0[["conditions"]], grepl("\\bdiab",DESCRIPTION, ignore.case = TRUE))%>%#grep will let you know the row number were the value is true
+    with
 
